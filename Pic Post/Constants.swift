981dbb7a60dea2fd7d33fb8378846cefc50ed0e7
Jake Bush <jackburton@Jakes-MacBook-Air.local>
2017-02-06 17:11:02 -0800

    Custom Sign in complete

---
+++ Pic Post/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pic Post
+//
+//  Created by Jake Bush on 2/6/17.
+//  Copyright © 2017 Henry Swanson. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
+
